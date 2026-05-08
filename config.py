873f0b233ea4d1c3f89faa55df76156784c3cd24
lordshiva09 <lordshivaiqoo@gmail.com>
2026-05-08 12:51:02 +0530

    fix env format

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# Load environment variables
 load_dotenv()
 
-# Telegram Config
-# Telegram Config
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
-CHANNEL_IDS = list(map(int, os.getenv("CHANNEL_IDS", "").split(","))) if os.getenv("CHANNEL_IDS") else []
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+CHANNEL_IDS = list(map(int, os.getenv("CHANNEL_IDS").split(",")))
 
 # Session Timings
 MORNING_SESSION_TIME = "10:00"
